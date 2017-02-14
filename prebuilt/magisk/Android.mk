738d59f52b5d409b38cac86cd2ccd82c07ef0dc8
mj1687 <alessandro.pasini12@gmail.com>
2017-02-14 22:00:45 +0100

    fix magisk build

--- prebuilt/magisk/Android.mk
+++ prebuilt/magisk/Android.mk
@@ -20,7 +20,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := MagiskManager
 LOCAL_MODULE_OWNER := elixium
-LOCAL_SRC_FILES := prebuilt/magisk/$(LOCAL_MODULE).apk
+LOCAL_SRC_FILES := MagiskManager.apk 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_MODULE_CLASS := APPS

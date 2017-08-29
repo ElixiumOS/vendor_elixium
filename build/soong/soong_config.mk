a12be640a4b9f3f73de26fb90c8d293e8eec82da
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-08-29 18:05:53 +0200

    [SQUASH] soong: ElixiumOS adaptation Fix soong custom config
    Soong flags: cleanup
    
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    
    soong: Rename our android package to android_jdc
    
    Including both android/soong/android and jdc/soong/android
    causes one to override the other. This isn't ideal behaviour
    so rename to avoid confliction.
    
    Change-Id: I93ab13e9bf4aa83ed79dee36db8dcb56ad215fad
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    
    soong_config: Add TARGET_USES_NON_TREBLE_CAMERA variable
    
    * Soong product variable 'uses_non_treble_camera'
    
    Change-Id: I38c01769dfeb376a1de474e99f0d3e1faaa6b15f
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    
    (2/2) Fix soong flags
    
    This time for real
    
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    
    soong: guys this is not JDC
    
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,8 +1,10 @@
-lineage_soong:
+# Insert new variables inside the Jdc structure
+elix_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
+	echo '"elix": {'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false)'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
